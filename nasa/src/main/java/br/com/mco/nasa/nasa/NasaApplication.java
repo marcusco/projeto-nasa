b1034b9be885acf44f89bae78a7b382653ef39c7
Marcus Cesar <marcuscesro@gmail.com>
2023-12-01 23:36:17 -0300

    Commit

--- nasa/src/main/java/br/com/mco/nasa/nasa/NasaApplication.java
+++ nasa/src/main/java/br/com/mco/nasa/nasa/NasaApplication.java
@@ -2,12 +2,11 @@ package br.com.mco.nasa.nasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 
 
 @SpringBootApplication
-@EnableFeignClients
 public class NasaApplication {
 
 	public static void main(String[] args) {
--- nasa/src/main/java/br/com/mco/nasa/nasa/service/EarthService.java
+++ nasa/src/main/java/br/com/mco/nasa/nasa/service/EarthService.java
@@ -4,19 +4,15 @@ import java.util.Date;
 
 import org.springframework.stereotype.Service;
 
-import br.com.mco.nasa.nasa.clients.EarthClient;
+
 
 @Service
 public class EarthService {
 
-    private EarthClient earthClient;
-    
-    public EarthService(EarthClient earthClient){
-        this.earthClient = earthClient;
-    }
+   
 
     public String getEarth(Double lat, Double lon, Date data, String apiKey){
-        return earthClient.getEarth(lat, lon, data, apiKey);
+        return null;
     }
     
 }
